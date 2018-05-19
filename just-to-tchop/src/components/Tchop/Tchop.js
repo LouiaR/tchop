d8ee1bce885f26753208f32efcc2d5eec5c444ad
louiaR <louitiani@gmail.com>
2018-05-19 21:06:35 +0100

    Dynamically calculating sum of ingredients

--- just-to-tchop/src/components/Tchop/Tchop.js
+++ just-to-tchop/src/components/Tchop/Tchop.js
@@ -5,11 +5,15 @@ import classes from './Tchop.css';
 
 const tchop = (props) => {
     const ingredients = props.ingredients;
-    const menu = Object.keys(ingredients)
+   let menu = Object.keys(ingredients)
         .map(ingre => {return [...Array(ingredients[ingre])]
         .map((_, i) => <Ingredients key={ingre+i} type={ingre} />
     )})
-    console.log(menu)    
+    const menuLength = [].concat.apply([], menu).length;
+    console.log(menuLength)
+    if (menuLength === 0){
+         menu = <p> Please add your favorite flavours! </p>;
+    }   
     return (
         <div className={classes.Burger}>
             <Ingredients type='bread-top'/>
