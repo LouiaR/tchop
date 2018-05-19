d17ddcd4d890151e83a35fd7ac1d9541204e95a7
louiaR <louitiani@gmail.com>
2018-05-19 19:16:00 +0100

    Add ingredient & validation

--- just-to-tchop/src/components/Layout/Layout.js
+++ just-to-tchop/src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/aux';
 
+import classes from './Layout.css';
+
 const layout = (props) => (
     <Aux>
         <div> top nav drawer </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
